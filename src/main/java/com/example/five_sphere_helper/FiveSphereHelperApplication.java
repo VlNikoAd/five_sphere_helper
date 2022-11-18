5e327715bc3ef7f98b2ce60c8db79d3a5346e4d1
VlNikoAd <nikolaenko_vladislav@icloud.com>
2022-11-18 09:15:31 +0300

    created basic app

---
+++ src/main/java/com/example/five_sphere_helper/FiveSphereHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.five_sphere_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveSphereHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiveSphereHelperApplication.class, args);
+    }
+
+}
